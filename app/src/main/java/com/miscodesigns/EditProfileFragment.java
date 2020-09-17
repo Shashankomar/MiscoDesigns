2208aa8b6eb390fd7f7e16328a8018eacc3eca04
Shashankomar <shashankomar786@gmail.com>
2020-09-17 14:50:34 +0530

    edit profile fragment transactions to test UI

--- app/src/main/java/com/miscodesigns/EditProfileFragment.java
+++ app/src/main/java/com/miscodesigns/EditProfileFragment.java
@@ -1,9 +1,11 @@
 package com.miscodesigns;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,7 +15,13 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Objects;
+
 public class EditProfileFragment extends Fragment {
+    private TextView mTvBtnEditUsername, mTvBtnEditBio, mTvBtnEditWebsite;
+    private TextView mCopyLink;
+    private LinearLayout snackbarView;
+
     public static EditProfileFragment newInstance() {
         return new EditProfileFragment();
     }
@@ -22,64 +30,26 @@ public class EditProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
-
         initView(view);
-
+        setClickListeners();
         return view;
     }
 
-    private void initView(View view) {
-
-        TextView copyLink = view.findViewById(R.id.tv_btn_copy_link);
-        final LinearLayout snackbarView = view.findViewById(R.id.ll_edit_profile);
-
-        copyLink.setOnClickListener(new View.OnClickListener() {
+    private void setClickListeners() {
+        mCopyLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               /* Snackbar snackbar1 = Snackbar.make(view.findViewById(android.R.id.content), "Link Copied!", Snackbar.LENGTH_SHORT);
-                snackbar1.show();*/
-
-                String message = "Link Copied!";
-                Snackbar snackbar = Snackbar.make(snackbarView, message, Snackbar.LENGTH_INDEFINITE);
-                snackbar.getView().setVisibility(View.INVISIBLE);
-                snackbar.setCallback(new Snackbar.Callback() {
-                    @Override
-                    public void onShown(Snackbar snackbar) {
-                        super.onShown(snackbar);
-                        snackbar.getView().setVisibility(View.VISIBLE);
-                    }
-                });
-                snackbar.show();
-
-                snackbar.getView().setVisibility(View.GONE);
-                snackbar.dismiss();
-
-               /* Snackbar snack = Snackbar.make(snackbarView, "Link Copied!", Snackbar.LENGTH_LONG);
+                Snackbar snack = Snackbar.make(snackbarView, "Link Copied!", Snackbar.LENGTH_LONG);
                 View view2 = snack.getView();
                 FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view2.getLayoutParams();
                 params.gravity = Gravity.TOP;
                 params.topMargin = 100;
-                Log.e("---f", String.valueOf(R.dimen.dp_40));
                 view2.setLayoutParams(params);
-                snack.show();*/
+                snack.show();
             }
         });
 
-       /* EditBioFragment nextFrag = new EditBioFragment();
-        Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fl_edit_profile, nextFrag, "findThisFragment")
-                .addToBackStack(null)
-                .commit();
-
-        EditWebsiteFragment nextFrag1 = new EditWebsiteFragment();
-        Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fl_edit_profile, nextFrag1, "findThisFragment1")
-                .addToBackStack(null)
-                .commit();
-*/
-
-        /*  View tv_btn = view.findViewById(R.id.tv_btn);
-        tv_btn.setOnClickListener(new View.OnClickListener() {
+        mTvBtnEditUsername.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditUserNameFragment nextFrag = new EditUserNameFragment();
@@ -88,6 +58,36 @@ public class EditProfileFragment extends Fragment {
                         .addToBackStack(null)
                         .commit();
             }
-        });*/
+        });
+
+        mTvBtnEditBio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditBioFragment nextFrag = new EditBioFragment();
+                Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fl_edit_profile, nextFrag, "findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        mTvBtnEditWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditWebsiteFragment nextFrag1 = new EditWebsiteFragment();
+                Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fl_edit_profile, nextFrag1, "findThisFragment1")
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    private void initView(View view) {
+        mTvBtnEditUsername = view.findViewById(R.id.tv_btn_change_username);
+        mTvBtnEditBio = view.findViewById(R.id.tv_btn_change_bio);
+        mTvBtnEditWebsite = view.findViewById(R.id.tv_btn_change_website);
+        mCopyLink = view.findViewById(R.id.tv_btn_copy_link);
+        snackbarView = view.findViewById(R.id.ll_edit_profile);
     }
 }
